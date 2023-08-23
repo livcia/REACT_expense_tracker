ea8d8472f1e3ebd95dae3d2cc2711903f0ca1f69
Oliwia <90615339+livcia@users.noreply.github.com>
2023-08-23 13:05:47 +0200

    get value from input text

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,13 +1,19 @@
 import React from "react"
+
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+
+    const titleChangeHandler = (event) => {
+        console.log(event.target.value)
+    }
+
     return(
         <form>
             <div className = "new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text"/>
+                    <input type="text" onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
