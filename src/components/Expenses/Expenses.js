361436b3f258d4d3189ed348fa0b3862ff2106ac
Oliwia <90615339+livcia@users.noreply.github.com>
2023-09-17 14:18:33 +0200

    add info about no expenses in year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 import React, { useState } from 'react';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -29,13 +29,7 @@ const Expenses = (props) => {
     <div>
     <Card className="expenses">
     <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-    {filteredExpenses.map(expense => 
-    <ExpenseItem 
-      key = {expense.id}
-      title = {expense.title} 
-      amount = {expense.amount} 
-      date = {expense.date}
-      />)}
+    <ExpensesList items={filteredExpenses}/>
     </Card>
     </div>
   );
