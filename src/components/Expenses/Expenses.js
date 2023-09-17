cd761c13c0cac1b59ff8b684018253d1c0a4f287
Oliwia <90615339+livcia@users.noreply.github.com>
2023-09-17 13:48:11 +0200

    dynamics sorting by years

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -21,13 +21,17 @@ const Expenses = (props) => {
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear)
   }
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  });
 
   return (
     <div>
     <Card className="expenses">
     <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-    {props.items.map(expense => 
+    {filteredExpenses.map(expense => 
     <ExpenseItem 
+      key = {expense.id}
       title = {expense.title} 
       amount = {expense.amount} 
       date = {expense.date}
